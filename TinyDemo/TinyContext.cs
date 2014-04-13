bb558d9445a706180a1e07cbad3c94fb40b2b48d
jdaley <joe@fastmail.com.au>
2014-04-13 15:59:44 +1000

    Example DB seed data

--- TinyDemo/TinyContext.cs
+++ TinyDemo/TinyContext.cs
@@ -9,6 +9,11 @@ namespace TinyDemo
 {
     public class TinyContext : DbContext
     {
+        static TinyContext()
+        {
+            Database.SetInitializer<TinyContext>(new TinyDatabaseInitializer());
+        }
+
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<User> Users { get; set; }
---
+++ TinyDemo/TinyDatabaseInitializer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using TinyDemo.Entities;
+
+namespace TinyDemo
+{
+    public class TinyDatabaseInitializer : DropCreateDatabaseAlways<TinyContext>
+    {
+        protected override void Seed(TinyContext context)
+        {
+            context.Customers.Add(new Customer
+            {
+                Name = "Coconut Co"
+            });
+
+            context.Customers.Add(new Customer
+            {
+                Name = "Ink Inc"
+            });
+
+            context.Users.Add(new User
+            {
+                UserName = "bob",
+                Password = "s3cr3t",
+                CustomerId = 1
+            });
+
+            context.Users.Add(new User
+            {
+                UserName = "jane",
+                Password = "p@ssword",
+                CustomerId = 1
+            });
+
+            context.Users.Add(new User
+            {
+                UserName = "jill",
+                Password = "u8Dj7VapL0esJhBv3",
+                CustomerId = 2
+            });
+        }
+    }
+}
