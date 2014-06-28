331bb21539e1942903144ff3ce65884d47429994
jdaley <joe@fastmail.com.au>
2014-06-28 16:55:48 +1000

    Automatically show demo.html on launch

--- WhopperDemo/Global.asax.cs
+++ WhopperDemo/Global.asax.cs
@@ -2,18 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Http;
-using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
 
-namespace TinyDemo
+namespace WhopperDemo
 {
-    public class WebApiApplication : System.Web.HttpApplication
+    public class Global : System.Web.HttpApplication
     {
-        protected void Application_Start()
-        {
-            GlobalConfiguration.Configure(WebApiConfig.Register);
-        }
-
         protected void Application_BeginRequest(object sender, EventArgs e)
         {
             if (Request.RawUrl == "/")
